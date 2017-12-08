96d2467d258b9069474689cd153ff8be8e0f66b5
yangjj <yangjinjin@qianbao.com>
2017-12-08 15:26:46 +0800

    我的面板-个人信息

--- src/views/my/personInfo/index.vue
+++ src/views/my/personInfo/index.vue
@@ -1,20 +1,31 @@
 <template>
-  <div class="p20">
-    <h3>个人信息</h3>
-    <el-form ref="form" :model="form" label-width="80px">
+  <div class="main">
+    <h3 class="pb20 border-bottom">个人信息</h3>
+    <!-- <el-form ref="form" :model="form" label-width="80px">
       <el-form-item label="用户名">
         <el-input
         placehoder="请输入用户名"
         v-model="form.username">
         </el-input>
       </el-form-item>
-      <el-form-item label=Email地址>
+      <el-form-item label="Email地址">
         <el-input v-model="form.email"></el-input>
       </el-form-item>
       <el-form-item label="角色">
         <el-input v-model="form.role"></el-input>
       </el-form-item>
-    </el-form>
+    </el-form> -->
+
+    <el-row :gutter="20" class="pt24 pb12 g9 f14">
+      <el-col :span="6"><div class="grid-content bg-purple">用户名</div></el-col>
+      <el-col :span="6"><div class="grid-content bg-purple">Email地址</div></el-col>
+      <el-col :span="6"><div class="grid-content bg-purple">角色</div></el-col>
+    </el-row>
+    <el-row :gutter="20" class="g3 f16">
+      <el-col :span="6"><div class="grid-content bg-purple">小燕子</div></el-col>
+      <el-col :span="6"><div class="grid-content bg-purple">1122334@163.com</div></el-col>
+      <el-col :span="6"><div class="grid-content bg-purple">admin</div></el-col>
+    </el-row>
   </div>
   </template>
 
