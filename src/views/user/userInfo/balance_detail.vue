ba584518c7985175c166f681de4b91d6106007d6
yangjj <yangjinjin@qianbao.com>
2017-12-12 16:39:28 +0800

    用户-用户列表-余额详情balance_detail.vue

---
+++ src/views/user/userInfo/balance_detail.vue
@@ -0,0 +1,278 @@
+<template>
+  <div class="app-container calendar-list-container">
+    <div class="border-bottom pb12">
+      <h3 class="">基本信息</h3>
+      <el-row :gutter="20" class="g9 f14">
+        <el-col :span="6"><div>真实姓名</div></el-col>
+        <el-col :span="6"><div>用户ID</div></el-col>
+        <el-col :span="6"><div>账户余额</div></el-col>
+      </el-row>
+      <el-row :gutter="20" class="g3 f16">
+        <el-col :span="6"><div>小燕子</div></el-col>
+        <el-col :span="6"><div>#####</div></el-col>
+        <el-col :span="6"><div>30,000.87</div></el-col>
+      </el-row>
+    </div>
+    <h3>账户余额详情</h3>
+    <el-table :key='tableKey' :data="listData" v-loading="listLoading" element-loading-text="给我一点时间" border fit highlight-current-row style="width: 450px; overflow: auto;">
+      <el-table-column align="center" label="序号" width="100">
+        <template scope="scope">
+          <span>1</span>
+        </template>
+      </el-table-column>
+      <el-table-column align="center" label="串码" width="150">
+        <template scope="scope">
+          <span>串码</span>
+        </template>
+      </el-table-column>
+      <el-table-column align="center" label="金额" width="200">
+        <template scope="scope">
+          <span>65,000.78</span>
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <div v-show="!listLoading" class="pagination-container">
+      <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page.sync="listQuery.page"
+        :page-sizes="[10,20,30, 50]" :page-size="listQuery.limit" layout="total, sizes, prev, pager, next, jumper" :total="total">
+      </el-pagination>
+    </div>
+  </div>
+</template>
+
+<script>
+import { fetchList, fetchPv } from '@/api/article'
+import waves from '@/directive/waves/index.js' // 水波纹指令
+import { parseTime } from '@/utils'
+
+const calendarTypeOptions = [
+      { key: 'CN', display_name: '中国' },
+      { key: 'US', display_name: '美国' },
+      { key: 'JP', display_name: '日本' },
+      { key: 'EU', display_name: '欧元区' }
+]
+
+// arr to obj
+const calendarTypeKeyValue = calendarTypeOptions.reduce((acc, cur) => {
+  acc[cur.key] = cur.display_name
+  return acc
+}, {})
+
+export default {
+  name: 'table_demo',
+  directives: {
+    waves
+  },
+  data() {
+    return {
+      listData: [
+        {
+          userId: '招小财',
+          tel: '152167267',
+          nameAuth: '2017-09-09 09:09:09',
+          authCard: '60天',
+          bandBind: '12345',
+          qbAddr: '还款中'
+        }, {
+          userId: '招小财',
+          tel: '152167267',
+          nameAuth: '2017-09-09 09:09:09',
+          authCard: '60天',
+          bandBind: '12345',
+          qbAddr: '还款中'
+        }, {
+          userId: '招小财',
+          tel: '152167267',
+          nameAuth: '2017-09-09 09:09:09',
+          authCard: '60天',
+          bandBind: '12345',
+          qbAddr: '还款中'
+        }
+      ],
+      total: null,
+      listLoading: true,
+      listQuery: {
+        page: 1,
+        limit: 20,
+        importance: undefined,
+        title: undefined,
+        type: '',
+        sort: '+id'
+      },
+      temp: {
+        id: undefined,
+        importance: 0,
+        remark: '',
+        timestamp: 0,
+        title: '',
+        type: '',
+        status: 'published'
+      },
+      importanceOptions: [1, 2, 3],
+      calendarTypeOptions,
+      sortOptions: [{ label: '按ID升序列', key: '+id' }, { label: '按ID降序', key: '-id' }],
+      statusOptions: ['published', 'draft', 'deleted'],
+      dialogFormVisible: false,
+      dialogStatus: '',
+      textMap: {
+        update: '编辑',
+        create: '创建'
+      },
+      dialogPvVisible: false,
+      pvData: [],
+      showAuditor: false,
+      tableKey: 0
+    }
+  },
+  filters: {
+    statusFilter(status) {
+      const statusMap = {
+        published: 'success',
+        draft: 'gray',
+        deleted: 'danger'
+      }
+      return statusMap[status]
+    },
+    typeFilter(type) {
+      return calendarTypeKeyValue[type]
+    }
+  },
+  created() {
+    this.getList()
+  },
+  methods: {
+    getList() {
+      this.listLoading = true
+      fetchList(this.listQuery).then(response => {
+        this.list = response.data.items
+        this.total = response.data.total
+        this.listLoading = false
+      })
+    },
+    handleFilter() {
+      this.listQuery.page = 1
+      this.getList()
+    },
+    handleSizeChange(val) {
+      this.listQuery.limit = val
+      this.getList()
+    },
+    handleCurrentChange(val) {
+      this.listQuery.page = val
+      this.getList()
+    },
+    timeFilter(time) {
+      if (!time[0]) {
+        this.listQuery.start = undefined
+        this.listQuery.end = undefined
+        return
+      }
+      this.listQuery.start = parseInt(+time[0] / 1000)
+      this.listQuery.end = parseInt((+time[1] + 3600 * 1000 * 24) / 1000)
+    },
+    handleModifyStatus(row, status) {
+      this.$message({
+        message: '操作成功',
+        type: 'success'
+      })
+      row.status = status
+    },
+    handleCreate() {
+      this.resetTemp()
+      this.dialogStatus = 'create'
+      this.dialogFormVisible = true
+    },
+    handleUpdate(row) {
+      this.temp = Object.assign({}, row)
+      this.dialogStatus = 'update'
+      this.dialogFormVisible = true
+    },
+    handleDelete(row) {
+      this.$notify({
+        title: '成功',
+        message: '删除成功',
+        type: 'success',
+        duration: 2000
+      })
+      const index = this.list.indexOf(row)
+      this.list.splice(index, 1)
+    },
+    create() {
+      this.temp.id = parseInt(Math.random() * 100) + 1024
+      this.temp.timestamp = +new Date()
+      this.temp.author = '原创作者'
+      this.list.unshift(this.temp)
+      this.dialogFormVisible = false
+      this.$notify({
+        title: '成功',
+        message: '创建成功',
+        type: 'success',
+        duration: 2000
+      })
+    },
+    update() {
+      this.temp.timestamp = +this.temp.timestamp
+      for (const v of this.list) {
+        if (v.id === this.temp.id) {
+          const index = this.list.indexOf(v)
+          this.list.splice(index, 1, this.temp)
+          break
+        }
+      }
+      this.dialogFormVisible = false
+      this.$notify({
+        title: '成功',
+        message: '更新成功',
+        type: 'success',
+        duration: 2000
+      })
+    },
+    resetTemp() {
+      this.temp = {
+        id: undefined,
+        importance: 0,
+        remark: '',
+        timestamp: 0,
+        title: '',
+        status: 'published',
+        type: ''
+      }
+    },
+    handleFetchPv(pv) {
+      fetchPv(pv).then(response => {
+        this.pvData = response.data.pvData
+        this.dialogPvVisible = true
+      })
+    },
+    handleDownload() {
+      require.ensure([], () => {
+        const { export_json_to_excel } = require('vendor/Export2Excel')
+        const tHeader = ['时间', '地区', '类型', '标题', '重要性']
+        const filterVal = ['timestamp', 'province', 'type', 'title', 'importance']
+        const data = this.formatJson(filterVal, this.list)
+        export_json_to_excel(tHeader, data, 'table数据')
+      })
+    },
+    formatJson(filterVal, jsonData) {
+      return jsonData.map(v => filterVal.map(j => {
+        if (j === 'timestamp') {
+          return parseTime(v[j])
+        } else {
+          return v[j]
+        }
+      }))
+    }
+  }
+}
+</script>
+<style media="screen" lang='scss'>
+
+  a {
+    color: #20a0ff;
+  }
+  a:hover {
+    color: red;
+    opacity: 0.4;
+    filter:alpha(opacity=40);
+  }
+</style>
