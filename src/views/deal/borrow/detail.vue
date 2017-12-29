be5a5268bc49140f638cddf9cf42f747d7dfc86c
yangjj <yangjinjin@qianbao.com>
2017-12-29 10:10:43 +0800

    完成页面src/views/deal/borrow/detail.vue

--- src/views/deal/borrow/detail.vue
+++ src/views/deal/borrow/detail.vue
@@ -61,6 +61,101 @@
         </div>
       </el-col>
     </el-row>
+    <div class="">
+      <h3 class="g3 f16 border-bottom">基本信息</h3>
+      <el-table :key='tableKey' :data="list" v-loading="listLoading" element-loading-text="给我一点时间" border fit highlight-current-row >
+      <!-- <el-table border fit highlight-current-row > -->
+        <el-table-column align="center" label="借款编号" width="120">
+          <template scope="scope">
+            <span>{{ scope.row.borrowId }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column align="center" label="申请产品" width="120">
+          <template scope="scope">
+            <span>信贷业务3</span>
+          </template>
+        </el-table-column>
+        <el-table-column align="center" label="产品利率" width="120">
+          <template scope="scope">
+            <span>10.8000%</span>
+          </template>
+        </el-table-column>
+        <el-table-column align="center" label="合同金额" width="120">
+          <template scope="scope">
+            <span>99</span>
+          </template>
+        </el-table-column>
+        <el-table-column align="center" label="借款申请额" width="120">
+          <template scope="scope">
+            <span>99</span>
+          </template>
+        </el-table-column>
+        <el-table-column align="center" label="实际放款金额" width="140">
+          <template scope="scope">
+            <span>99</span>
+          </template>
+        </el-table-column>
+        <el-table-column align="center" label="借款期限" width="120">
+          <template scope="scope">
+            <span>3个月</span>
+          </template>
+        </el-table-column>
+        <el-table-column align="center" label="还款方式" width="120">
+          <template scope="scope">
+            <span>一次性还本付息</span>
+          </template>
+        </el-table-column>
+        <el-table-column align="center" label="标的状态" width="120">
+          <template scope="scope">
+            <span>还款中</span>
+          </template>
+        </el-table-column>
+        <el-table-column align="center" label="申请时间" width="120">
+          <template scope="scope">
+            <span>2017-09-19 05:48:40</span>
+          </template>
+        </el-table-column>
+        <el-table-column align="center" label="放款时间" width="120">
+          <template scope="scope">
+            <span>2017-09-19 05:48:40</span>
+          </template>
+        </el-table-column>
+        <el-table-column align="center" label="支付订单号" width="120">
+          <template scope="scope">
+            <span>9######</span>
+          </template>
+        </el-table-column>
+        <el-table-column align="center" label="所在区块" width="120">
+          <template scope="scope">
+            <span>8769</span>
+          </template>
+        </el-table-column>
+        <el-table-column align="center" label="交易哈希" width="120">
+          <template scope="scope">
+            <span>ffhjffhj</span>
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
+    <div class="">
+      <h3 class="g3 f16 border-bottom">附加信息</h3>
+      <el-row :gutter="100">
+        <el-col :span="8">
+          <div class="grid-content lh50">
+            <span class="weight-500 g3">授信</span>
+            <span class="padding50 g6">授信全要素</span>
+            <router-link to="" class="blue">查看</router-link>
+          </div>
+        </el-col>
+        <el-col :span="8">
+          <div class="grid-content lh50">
+            <span class="weight-500 g3">借款合同</span>
+            <span class="padding50 g6">PDF</span>
+            <router-link to="" class="blue">查看</router-link>
+          </div>
+        </el-col>
+      </el-row>
+    </div>
   </div>
   </template>
 
@@ -72,7 +167,12 @@
             username: '',
             email: '',
             role: ''
-          }
+          },
+          list: [
+            {
+              borrowId: 12334
+            }
+          ]
         }
       }
     }
@@ -83,6 +183,9 @@
   }
   .bg-title {
     background: #eef1f6;
+  }
+  .padding50 {
+    padding: 50px;
   }
   /*.bg-purple {
     background: #d3dce6;
